9c2fdbf341668c0ca2b2ba96ec9bec73ebe80c85
chanchi- <chanchi0707@gmail.com>
2017-01-07 13:57:14 +0900

    <Procider>を入れずにHello,World

---
+++ src/js/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Provider from 'redux'
+
+export default class Container extends React.Component {
+	render(){
+		return(
+			<div>
+				<h1>Hello,World</h1>
+			</div>
+		);
+	}
+}
